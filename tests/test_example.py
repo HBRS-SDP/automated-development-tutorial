a0e1b3d5e340e23208fae3dd71e15dfc25ec4453
Minh Nguyen <1168534+minhnh@users.noreply.github.com>
2023-06-05 15:12:24 +0200

    add tests

--- tests/test_example.py
+++ tests/test_example.py
@@ -6,9 +6,11 @@ class TestExample(unittest.TestCase):
     def test_fibonacci(self):
         self.assertEqual(fibonacci(5), [1, 1, 2, 3, 5])
         self.assertEqual(fibonacci(6), [1, 1, 2, 3, 5])
+        self.assertEqual(fibonacci(1), [1])
 
     def test_sum_of_primes(self):
         self.assertEqual(sumOfPrimes(10), 17)
+        self.assertEqual(sumOfPrimes(-1), 0)
 
 
 if __name__ == "__main__":
